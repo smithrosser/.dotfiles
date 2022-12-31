0b2bf2c7caf90622c7692b8fa197bf89b11a1d14
Tom <tom@ubuntom>
2022-12-31 16:59:58 +0000

    add a couple of dotfiles

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables
+# ----------------------------------------------------------------------------------------
+
+USER_HOME="/home/tom"
+ZSHRC_PATH="${USER_HOME}/.zshrc"
+VSCODE_SETTINGS_PATH="${USER_HOME}/.config/Code/User/settings.json"
+ALACRITTY_SETTINGS_PATH="${USER_HOME}/.config/alacritty/alacritty.yml"
+
+# Remove original files, create symbolic links to here
+# ----------------------------------------------------------------------------------------
+
+# .zshrc
+rm ${ZSHRC_PATH}
+ln -s ${PWD}/.zshrc ${ZSHRC_PATH}
+
+# VSCode settings
+rm ${VSCODE_SETTINGS_PATH}
+ln -s ${PWD}/settings.json ${VSCODE_SETTINGS_PATH}
+
+# Alacritty settings
+rm ${ALACRITTY_SETTINGS_PATH}
+ln -s ${PWD}/alacritty.yml ${ALACRITTY_SETTINGS_PATH}
